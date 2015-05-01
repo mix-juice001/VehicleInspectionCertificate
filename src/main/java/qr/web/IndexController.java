1482e430dfb8f8ae259ab192f8aee4199a633b29
takumasetoguchi <setoguchi.takuma@gmail.com>
2015-05-01 13:32:58 +0900

    initial commit

---
+++ src/main/java/qr/web/IndexController.java
@@ -0,0 +1,14 @@
+package qr.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    String start() {
+        return "webcamera";
+    }
+}
